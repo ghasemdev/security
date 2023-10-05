e19ac42565d4b60db0af5773d6c243e77e3961d4
Ghasem Shirdel <shirdelghasem79@gmail.com>
2023-10-05 23:20:09 +0330

    feat: add secure shared pref

--- app/src/main/java/com/example/security/MainActivity.kt
+++ app/src/main/java/com/example/security/MainActivity.kt
@@ -1,21 +1,37 @@
 package com.example.security
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.core.content.edit
+import dagger.hilt.android.AndroidEntryPoint
 import com.example.security.ui.theme.AppTheme
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+  @Inject
+  lateinit var sharedPref: SharedPreferences
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     // including IME animations, and go edge-to-edge
@@ -24,13 +40,38 @@ class MainActivity : ComponentActivity() {
 
     setContent {
       val darkTheme = isSystemInDarkTheme()
+      var key by remember { mutableStateOf("") }
+      var value by remember { mutableStateOf("") }
+
+      var text by remember { mutableStateOf("") }
 
       AppTheme(darkTheme = darkTheme) {
-        Box(
-          modifier = Modifier.fillMaxSize(),
-          contentAlignment = Alignment.Center
+        Column(
+          modifier = Modifier
+            .fillMaxSize()
+            .systemBarsPadding(),
+          horizontalAlignment = Alignment.CenterHorizontally,
+          verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-          Text(text = stringFromJNI(), style = MaterialTheme.typography.h3)
+          TextField(value = key, onValueChange = { key = it }, label = { Text(text = "key") })
+          TextField(value = value, onValueChange = { value = it }, label = { Text(text = "value") })
+          Button(
+            onClick = {
+              sharedPref.edit {
+                putString(key, value)
+              }
+            }
+          ) {
+            Text(text = "save")
+          }
+          Button(
+            onClick = {
+              text = sharedPref.getString(key, "").orEmpty()
+            }
+          ) {
+            Text(text = "load")
+          }
+          Text(text = text)
         }
       }
 
