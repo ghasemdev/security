3aa7a1e4b57321980356163f02cebc3612d8a692
Ghasem Shirdel <shirdelghasem79@gmail.com>
2024-02-08 21:46:47 +0330

    refactor: remove unused class

--- app/src/main/java/com/example/security/json/Entries.kt
+++
@@ -1,8 +0,0 @@
-package com.example.security.json
-
-data class Entries<K, V>(val value: List<Entry<K, V>>)
-
-fun sortedEntries(vararg entries: Entry<String, Any?>): Entries<String, Any?> {
-  val map = entries.toMutableList()
-  return Entries(map.sortedBy { it.key })
-}
--- app/src/main/java/com/example/security/json/Entry.kt
+++
@@ -1,5 +0,0 @@
-package com.example.security.json
-
-data class Entry<K, V>(val key: K, val value: V)
-
-infix fun <A, B> A.to(that: B): Entry<A, B> = Entry(this, that)
